637e88577a7192019a9bfd279205a52a7218ee7a
NhonDCSE173131 <NhonDCSE173131@fpt.edu.vn>
2024-11-07 18:32:05 +0700

    UI Flower

--- project/src/main/java/hsf301/fe/project/pojo/Flower.java
+++ project/src/main/java/hsf301/fe/project/pojo/Flower.java
@@ -1,11 +1,7 @@
 package hsf301.fe.project.pojo;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
 import lombok.*;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 
 @Entity
 @Table(name = "flowers")
@@ -18,22 +14,19 @@ public class Flower {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-    @NotBlank(message = "Flower name is required")
-    @Size(max = 100, message = "Flower name must be less than 100 characters")
-    @Column(nullable = false)
+    @Column(nullable = false, length = 100)
     private String name;
 
-    private String type; 
-    private int quantity; 
+    private String type;
+    private int quantity;
     private String condition;
 
-    @NotNull(message = "Price price is required")
-    @Positive(message = "Price price must be positive")
     private double price;
+
     @Column(name = "image_url")
     private String imageUrl;
 
     @ManyToOne
-    @JoinColumn(name = "seller_id") 
+    @JoinColumn(name = "seller_id")
     private Users seller;
 }
